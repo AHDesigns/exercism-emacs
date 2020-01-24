83890d0723c8d771d50d6cce7503966c26886243
Adam Hall <adamhalldesigns@gmail.com>
2020-01-24 07:47:48 +0000

    add two-fer

---
+++ two-fer/two-fer.el
@@ -0,0 +1,11 @@
+;;; two-fer.el --- Two-fer Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+(defun two-fer (&optional name)
+  (unless name (setq name "you"))
+  (format "One for %s, one for me." name))
+
+(provide 'two-fer)
+;;; two-fer.el ends here
